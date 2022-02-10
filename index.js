687e2c9c28f612e14483445285a6be98e3b03ae9
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-02-10 10:21:37 -0600

    commit tree 74ea0394462722cfac422f4d414b88637b328a07

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(names => names.toLowerCase() === name.toLowerCase());
+}    
+
+function fuzzyMatch(drivers, letter) {
+    return drivers.filter(names => names[0].toLowerCase() === letter[0].toLowerCase());
+}
+
+function matchName (drivers, name) {
+    return drivers.filter(names => names.name.toLowerCase() === name.toLowerCase());
+}
